d5b0fc36d875cad747e3aed1235a6951a6834090
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-05-25 10:28:22 -0400

    wrote main menu launcher

---
+++ index.js
@@ -0,0 +1,29 @@
+const { prompt } = require('inquirer')
+
+const mainMenuLauncher = () => {
+  const mainMenuChoices = ['Add Department', 'Add Role', 'Add Employee', 'View Departments', 'View Roles', 'View Employees', 'Update Employee Role']
+
+  const mainMenuPrompt = {
+    type: 'list',
+    name: 'choice',
+    message: 'What would you like to do?',
+    choices: mainMenuChoices
+  }
+  //Launches the main menu
+  prompt(mainMenuPrompt)
+    .then(({ choice }) => {
+      //Choice: a string from the mainMenuChoices array
+      //Remove capitals
+      choice = choice.toLowerCase()
+      //Split it into useful parameters
+      let [op, table, ...others] = choice.split(' ')
+      //Remove plurals
+      if (table[table.length - 1] == 's') {
+        table = table.substring(0, table.length - 1)
+      }
+      actionHandler(op, table, others)
+    })
+    .catch(err => console.log(err))
+}
+
+module.exports = mainMenuLauncher
